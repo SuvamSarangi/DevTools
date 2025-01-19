a4836ab749834ec44971a10fc60d794c927d0318
subham <subhamsarangi23@gmail.com>
2025-01-19 21:50:42 +0530

    TL changes main method

--- GitProj01/src/main/java/com/nt/test/App.java
+++ GitProj01/src/main/java/com/nt/test/App.java
@@ -1,10 +1,14 @@
 package com.nt.test;
 
+import com.nt.service.Arithmatic;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Arithmatic ar = new Arithmatic();
+      long result =  ar.sub(10, 20);
+       System.out.println("result of sum: "+result);
     }
 }
